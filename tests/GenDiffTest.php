5e4144365155373ab2c5bc8729be2c98ec567f41
Vladimir Ugarov <graciadan@rambler.ru>
2022-09-17 21:27:22 +0300

    add json.txt & change GenDiffTest.php

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -58,8 +58,10 @@ class GenDiffTest extends TestCase
     {
         $expected1 = file_get_contents('tests/fixtures/stylish.txt');
         $expected2 = file_get_contents('tests/fixtures/plain.txt');
+        $expected3 = file_get_contents('tests/fixtures/json.txt');
 
         $this->assertEquals($expected1, genDiff($this->files['json1'], $this->files['json2']));
         $this->assertEquals($expected2, genDiff($this->files['yaml1'], $this->files['yaml2'], 'plain'));
+        $this->assertEquals($expected3, genDiff($this->files['yaml1'], $this->files['yaml2'], 'json'));
     }
 }
