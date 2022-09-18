5fe6841f1b58fcc0fcaef9e5e895e520055a1558
Vladimir Ugarov <graciadan@rambler.ru>
2022-09-19 01:31:55 +0300

    fix parsers

--- src/Parsers.php
+++ src/Parsers.php
@@ -7,17 +7,20 @@ use Symfony\Component\Yaml\Yaml;
 function parse(string $path): object
 {
     $content = file_get_contents($path);
+    if ($content === false) {
+        throw new \Exception("Error file reading {$path}");
+    }
+
     $type = pathinfo($path, PATHINFO_EXTENSION);
+
     switch ($type) {
         case 'json':
             return json_decode($content, false);
-            break;
         case 'yaml':
         case 'yml':
             return Yaml::parse($content, Yaml::PARSE_OBJECT_FOR_MAP);
-            break;
         default:
             throw new \Exception("Unknown file type {$type}");
-            break;
+
     }
 }
