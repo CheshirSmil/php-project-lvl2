c9c3fb64ac7f120d7db721d8336e9cffa28f3e70
Vladimir Ugarov <graciadan@rambler.ru>
2023-01-20 00:37:42 +0300

    refactoring GenDiffTest

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -6,35 +6,45 @@ use PHPUnit\Framework\TestCase;
 
 use function Differ\Differ\genDiff;
 
-function getFixturePath(string $fileName): string
-{
-    return implode(DIRECTORY_SEPARATOR, [__DIR__, "fixtures", $fileName]);
-}
-
 class GenDiffTest extends TestCase
 {
+    private function getFixturePath(string $fileName): string
+    {
+        return implode(DIRECTORY_SEPARATOR, [__DIR__, "fixtures", $fileName]);
+    }
+
     /**
      * @dataProvider additionProvider
      */
 
-    public function testGenDiff($pathToFile1, $pathToFile2, $format, $expected)
+    public function testGenDiff($expected, $pathToFile1, $pathToFile2, $format = 'stylish'): void
     {
-        $pathToExpectedFixture = getFixturePath($expected);
-        $this->assertStringEqualsFile(
-            $pathToExpectedFixture,
-            genDiff(getFixturePath($pathToFile1), getFixturePath($pathToFile2), $format)
-        );
+        $this->assertStringEqualsFile($expected, genDiff($pathToFile1, $pathToFile2, $format));
     }
 
     public function additionProvider()
     {
+        $stylishFormat = 'stylish';
+        $plainFormat = 'plain';
+        $jsonFormat = 'json';
+
+        $expectedStylish = $this->getFixturePath("stylish.txt");
+        $expectedPlain = $this->getFixturePath("plain.txt");
+        $expectedJson = $this->getFixturePath("json.txt");
+        $file1Json = $this->getFixturePath('file1.json');
+        $file2Json = $this->getFixturePath('file2.json');
+        $file1Yaml = $this->getFixturePath('file1.yaml');
+        $file2Yml = $this->getFixturePath('file2.yml');
+
         return [
-            ['file1.json', 'file2.json', 'stylish', 'stylish.txt'],
-            ['file1.json', 'file2.json', 'plain', 'plain.txt'],
-            ['file1.json', 'file2.json', 'json', 'json.txt'],
-            ['file1.yaml', 'file2.yml', 'stylish', 'stylish.txt'],
-            ['file1.yaml', 'file2.yml', 'plain', 'plain.txt'],
-            ['file1.yaml', 'file2.yml', 'json', 'json.txt']
+            [$expectedStylish, $file1Json, $file2Json],
+            [$expectedStylish, $file1Yaml, $file2Yml],
+            [$expectedStylish, $file1Json, $file2Json, $stylishFormat],
+            [$expectedStylish, $file1Yaml, $file2Yml, $stylishFormat],
+            [$expectedPlain, $file1Json, $file2Json, $plainFormat],
+            [$expectedPlain, $file1Yaml, $file2Yml, $plainFormat],
+            [$expectedJson, $file1Json, $file2Json, $jsonFormat],
+            [$expectedJson, $file1Yaml, $file2Yml, $jsonFormat],
         ];
     }
 }
